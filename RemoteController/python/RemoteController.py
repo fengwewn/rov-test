191d96dc998658c0abeaa8ca779218824948b519
mkcoldwolf <32544898+mkcoldwolf@users.noreply.github.com>
2021-11-28 19:49:00 +0800

    Update python client for remote control

--- RemoteController/python/RemoteController.py
+++ RemoteController/python/RemoteController.py
@@ -1,22 +1,48 @@
 import asyncio
 import websockets
 import json
+import time
 
-def processMsg(msg):
-    jsonObj = json.loads(msg)
+class RemoteController:
 
-    if jsonObj["action"] == "updateJoyStick":
-        if jsonObj["stick"] == "turn":
-            print("Turn: " + str(jsonObj["data"]))
-        elif jsonObj["stick"] == "forward":
-            print("Forward: " + str(jsonObj["data"]))
+    def __init__(self, TimedController):
+        self.tc = TimedController
+        self.lc = self.tc.LowController
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.startWs())
 
-async def hello():
-    uri = "wss://sites.google.com/view/juneer2021fall/websocket/?type=nightCrystal"
-    async with websockets.connect(uri) as websocket:
-        while True:
-            msg = await websocket.recv()
-            processMsg(msg)
+    def processMsg(self, msg):
+        print(msg)
+        jsonObj = json.loads(msg)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(hello())
+        if jsonObj["action"] == "updateJoyStick":
+            if jsonObj["stick"] == "turn":
+                data = jsonObj["data"]
+                if data > 0:
+                    if data > 100: data = 100
+                    self.lc.rotateClockwise(data)
+                elif data < 0:
+                    if data < -100: data = -100
+                    self.lc.rotateAntiClockwise(-data)
+                else:
+                    self.lc.stop()
+            elif jsonObj["stick"] == "forward":
+                data = jsonObj["data"]
+                if data > 0:
+                    if data > 100: data = 100
+                    self.lc.forwards(data)
+                elif data < 0:
+                    if data < -100: data = -100
+                    self.lc.backwards(-data)
+                else:
+                    self.lc.stop()
+        
+        if jsonObj["action"] == "updateObjShipper":
+            self.lc.moveLifter(jsonObj["data"])
+
+    async def startWs(self):
+        uri = "wss://sites.google.com/view/juneer2021fall/websocket/?type=project_haws"
+        async with websockets.connect(uri) as websocket:
+            while True:
+                msg = await websocket.recv()
+                self.processMsg(msg)
